4494da1960b234f8b71a8944bdab0c26be7b60d3
Developer <developer@example.com>
2025-10-04 11:46:58 +0300

    woring display

--- libraries/ESP32_LCD_Drivers/Backlight.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define LCD_Backlight_PIN   5
-#define PWM_Frequency       20000   
-#define PWM_Resolution      10       
-
-void Backlight_Init();
-void Set_Backlight(uint8_t brightness_percent);
